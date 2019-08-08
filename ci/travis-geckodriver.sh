1fea6ec5549503748963afeede0ea52c1ef11ca1
Sean Chalmers <sclhiannan@gmail.com>
2019-08-08 11:32:06 +1000

    Fix up the geckodriver travis setup script

--- ci/travis-geckodriver.sh
+++ ci/travis-geckodriver.sh
@@ -5,5 +5,6 @@ G_PATH="/usr/local/bin"
 
 echo "wget'ing geckodriver"
 wget "https://github.com/mozilla/geckodriver/releases/download/$G_VER/geckodriver-${G_VER}-linux64.tar.gz" -P "/tmp/"
-tar -xvf "/tmp/geckodriver-${G_VER}-linux64.tar.gz"
-mv /tmp/geckodriver /usr/local/bin/geckodriver
+cd /tmp
+tar xvf "geckodriver-${G_VER}-linux64.tar.gz"
+mv geckodriver /usr/local/bin/geckodriver
